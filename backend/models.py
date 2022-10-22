c14024c8a82c667b0b66b13c9006352d3e58cc24
LenaNguyen27 <nguyen.lena27@hotmail.com>
2022-10-22 14:55:50 -0400

    add indexes and make fields unique

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
-    youtube_id = models.CharField(max_length=11)
+    youtube_id = models.CharField(max_length=11, unique=True)
     title = models.CharField(max_length=100)
 
 class Clip(models.Model):
@@ -15,14 +15,20 @@ class Clip(models.Model):
         ordering = ['timestamp']
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     value = models.CharField(max_length=50)
     clips = models.ManyToManyField(Clip)
+	
+    class Meta:
+        indexes = [models.Index(fields=['name']), ]
 
 class ClipTags(models.Model):
     clip = models.ForeignKey(Clip, on_delete=models.CASCADE, related_name='cliptags')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
 class TagGroup(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     tags = models.ManyToManyField(Tag)
+    
+    class Meta:
+        indexes = [models.Index(fields=['name']), ]
