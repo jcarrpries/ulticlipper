0b3040d5d815bc1fca15884f215ff7fa0cf4f9bf
Flynn Gurnsey <fjbgurns@uwaterloo.ca>
2023-02-26 22:04:55 -0500

    fixed backend tests to work with auth

--- backend/test_clip_list.py
+++ backend/test_clip_list.py
@@ -1,28 +1,45 @@
 from rest_framework.test import APITestCase
 
 from .models import Clip, Tag, TagGroup, Video
+from django.contrib.auth.models import User
 
 class ClipListTestCase(APITestCase):
+    def setUpAuth(self):
+        resp = self.client.post('/api/createuser/', {
+            "email": "user@gmail.com",
+            "password": "password123",
+            "display_name": "John",
+        })
+
+        self.client.post('/api/create_team/', {
+            'team_name': 'Team A',
+        })
+
+        player_id = resp.json()['id']
+        return player_id
+
     def setUp(self):
+        player_id = self.setUpAuth()
+        team = User.objects.get(id=player_id).user_data.active_team
         # load some clips into the database for querying, with some tags
-        video, _ = Video.objects.get_or_create(youtube_id='test', title='frisbee')
-        clip, _ = Clip.objects.get_or_create(timestamp=0, duration=0, video=video)
-        tag_group, _ = TagGroup.objects.get_or_create(name='players_on')
-        tag, _ = Tag.objects.get_or_create(name='james')
+        video, _ = Video.objects.get_or_create(youtube_id='test', title='frisbee', team=team)
+        clip, _ = Clip.objects.get_or_create(timestamp=0, duration=0, video=video, team=team)
+        tag_group, _ = TagGroup.objects.get_or_create(name='players_on', team=team)
+        tag, _ = Tag.objects.get_or_create(name='james', team=team)
         tag_group.tags.add(tag)
         tag.clips.add(clip)
-        
-        clip, _ = Clip.objects.get_or_create(timestamp=1, duration=1, video=video)
+
+        clip, _ = Clip.objects.get_or_create(timestamp=1, duration=1, video=video, team=team)
 
     def test_no_params(self):
         resp = self.client.get('/api/clips/?')
         self.assertEqual(len(resp.data), 2)
-    
+
     def test_param(self):
         james_tag = Tag.objects.get(name='james')
         resp = self.client.get(f'/api/clips/?players_on=[{james_tag.id}]&')
         self.assertEqual(len(resp.data), 1)
-    
+
     def test_no_clips_found(self):
         resp=self.client.get(f'/api/clips/?players_on=[99]&')
         self.assertEqual(len(resp.data), 0)
--- backend/test_clips_by_video.py
+++ backend/test_clips_by_video.py
@@ -1,19 +1,37 @@
 from rest_framework.test import APITestCase
 
 from .models import Clip, Tag, TagGroup, Video
+from django.contrib.auth.models import User
 
 class ClipVideosTestCase(APITestCase):
+    def setUpAuth(self):
+        resp = self.client.post('/api/createuser/', {
+            "email": "user@gmail.com",
+            "password": "password123",
+            "display_name": "John",
+        })
+
+        self.client.post('/api/create_team/', {
+            'team_name': 'Team A',
+        })
+
+        player_id = resp.json()['id']
+        return player_id
+
     def setUp(self):
+        player_id = self.setUpAuth()
+        team = User.objects.get(id=player_id).user_data.active_team
+
         # load some clips into the database for querying, with some tags
-        video, _ = Video.objects.get_or_create(youtube_id='test', title='frisbee')
-        clip, _ = Clip.objects.get_or_create(timestamp=0, duration=0, video=video)
-        tag_group, _ = TagGroup.objects.get_or_create(name='event_type')
-        tag, _ = Tag.objects.get_or_create(name='CATCH')
+        video, _ = Video.objects.get_or_create(youtube_id='test', title='frisbee', team=team)
+        clip, _ = Clip.objects.get_or_create(timestamp=0, duration=0, video=video, team=team)
+        tag_group, _ = TagGroup.objects.get_or_create(name='event_type', team=team)
+        tag, _ = Tag.objects.get_or_create(name='CATCH', team=team)
         tag_group.tags.add(tag)
         tag.clips.add(clip)
-        
-        clip, _ = Clip.objects.get_or_create(timestamp=1, duration=0, video=video)
-        tag, _ = Tag.objects.get_or_create(name='DROP')
+
+        clip, _ = Clip.objects.get_or_create(timestamp=1, duration=0, video=video, team=team)
+        tag, _ = Tag.objects.get_or_create(name='DROP', team=team)
         tag_group.tags.add(tag)
         tag.clips.add(clip)
 
